27ff6b99ea83d43d5c2a9031d0ee2a3b525a041b
16kwin <DKM_PSVU@mail.ru>
2026-07-31 15:28:37 +0500

    3

--- vite.config.js
+++ vite.config.js
@@ -16,25 +16,6 @@ export default defineConfig({
       'localhost',
       '127.0.0.1',
       '45.146.164.123'
-    ],
-    proxy: {
-      '/v1': {
-        target: 'http://45.146.164.123:8086',
-        changeOrigin: true
-      },
-      '/api': {
-        target: 'http://45.146.164.123:8086',
-        changeOrigin: true
-      },
-      '/uploads': {
-        target: 'http://45.146.164.123:8086',
-        changeOrigin: true
-      },
-      '/ws-orders': {
-        target: 'ws://45.146.164.123:8086',
-        ws: true,
-        changeOrigin: true
-      }
-    }
+    ]
   }
 })
